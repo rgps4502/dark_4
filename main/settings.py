b0ae66b2a93da83a7842b63dd1c26f53331397dc
uobik <uobik@localhost.localdomain>
2023-07-03 13:17:32 +0800

    修改資料庫使用postgresql

--- main/settings.py
+++ main/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django',
+        'USER': 'postgres',
+        'PASSWORD': 'django1qaz#EDC',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
